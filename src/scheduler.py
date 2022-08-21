2422f6ebba2c727166b8f5dea396f024e7c71869
Amruth Nare <45131882+amruth21@users.noreply.github.com>
2022-08-21 14:02:48 -0400

    schedule testing with heroku

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,25 @@
 from botV1 import *
 from trends import *
+import schedule
+import time
 
+print("Scheduler Running...")
 
-print("testttt")
-createTweet("United Kingdom")
+def jobUSA():
+    createTweet("United States")
+
+def jobUK():
+    createTweet("United Kingdom")
+
+def jobCA():
+    createTweet("Canada")
+
+schedule.every().day.at("18:10").do(jobUSA)
+
+schedule.every().day.at("21:00").do(jobUSA)
+schedule.every().day.at("21:15").do(jobUK)
+schedule.every().day.at("21:30").do(jobCA)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
