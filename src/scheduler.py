220595d5c3284558f20dea9e0bdf8037cf121263
Amruth Nare <45131882+amruth21@users.noreply.github.com>
2022-08-21 23:19:09 -0400

    Update scheduler.py

--- src/scheduler.py
+++ src/scheduler.py
@@ -15,9 +15,9 @@ def jobCA():
     createTweet("Canada")
 
 #Heroku servers 4 hours ahead
-schedule.every().day.at("20:00").do(jobUSA) #Posts at 7:00pm EST
-schedule.every().day.at("20:15").do(jobUK) #Posts at 7:15pm EST
-schedule.every().day.at("20:30").do(jobCA) #Posts at 7:30pm EST
+schedule.every().day.at("23:00").do(jobUSA) #Posts at 7:00pm EST
+schedule.every().day.at("23:15").do(jobUK) #Posts at 7:15pm EST
+schedule.every().day.at("23:30").do(jobCA) #Posts at 7:30pm EST
 
 while True:
     schedule.run_pending()
